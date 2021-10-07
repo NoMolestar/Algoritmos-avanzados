4f45771e6b4e0a09ed1cdec2eb9dfbaadc203aeb
Carlos Moisés Chávez Jiménez <carlosmoiseschavezjimenez@gmail.com>
2021-10-07 00:39:58 -0500

    Added the function and the tests cases

--- A01114901-A01637322_Act3.3/main.cpp
+++ A01114901-A01637322_Act3.3/main.cpp
@@ -8,10 +8,52 @@
 
 using namespace std;
 
-//Explicacion
-//Complejidad O()
-void Knapsack(vector<int> &valores, vector<int> &pesos, int pesoMax)
+//Función auxiliar que utiliza recursividad para regresar el número de valores de la mochila correspondientes
+//con el peso de los elementos y el peso maximo.
+//Complejidad: 0(2^n) siendo n el numero de elementos en la mochila.
+int auxMochila(vector<int> &valores, vector<int> &pesos, int pesoMax, int size)
 {
+    //Si el peso maximo es más pequeño que cero, regresamos un número negativo grande para indicar
+    //que se ha superado el limite de peso.
+    if (pesoMax < 0)
+    {
+        return -9999999;
+    }
+    //Si se ha alcanzado el limite de peso, o bien, se ha alcanzado el total de numero de elementos, se regresa 0.
+    if (size < 0 || pesoMax == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        //Agregamos el valor a la mochila y le sumamos el valor que sigue en el vector llamando a la funcion
+        //recursivamente. Esto restandole el peso del valor agregado al peso maximo.
+        int agregado = valores[size] + auxMochila(valores, pesos, pesoMax - pesos[size], size - 1);
+        //A la par de lo anterior, ignoramos el valor actual y no lo agregamos. Pasamos al siguiente valor
+        //enviandolo a la recursividad.
+        int noAgregado = auxMochila(valores, pesos, pesoMax, size - 1);
+        //Al final comparamos los resultados de ambos procesos recursivos para obtener el máximo de los valores que
+        //se obtuvieron dentro de la mochila y sin superar el peso maximo.
+        return max(agregado, noAgregado);
+    }
+}
+
+//Función principal que mandá llamar a la auxiliar que será la que regresará los valores dentro
+//de la mochila.
+//Complejidad O(1).
+void mochila(vector<int> &valores, vector<int> &pesos, int pesoMax, int size)
+{
+    //Si el numero de valores es 0, o bien, el peso maximo es 0. Entonces solo imprimimos el 0
+    if (size == 0 || pesoMax == 0)
+    {
+        cout << "0" << endl;
+    }
+    else
+    {
+        //Llamamos a la funcion auxiliar que nos permitira la recursividad
+        cout << endl
+             << auxMochila(valores, pesos, pesoMax, size - 1) << endl;
+    }
 }
 
 int main()
@@ -32,12 +74,10 @@ int main()
     for (int i = 0; i < n; i++)
     {
         cin >> pesoMax;
-        valores.push_back(pesoMax);
+        pesos.push_back(pesoMax);
     }
     cin >> pesoMax;
     //Pasamos la matriz a las funciones correspondientes
-    cout << endl;
-    Knapsack(valores, pesos, pesoMax);
-    cout << endl;
+    mochila(valores, pesos, pesoMax, n);
     return 0;
 }
--- A01114901-A01637322_Act3.3/readme.md
+++ A01114901-A01637322_Act3.3/readme.md
@@ -21,45 +21,87 @@ Salida: 3
 
 ### Segundo caso de Prueba
 
-3
-1
-2
-3
-4
 5
-1
-4
+24
+13
+23
+15
+16
+12
+7
+11
+8
+9
+26
 
-Salida:
+Salida: 51
 
-#### Explicación: Aumentamos el tamaño de la matriz para verificar su funcionamiento en matrices con vertices impares
+#### Explicación: Probar el funcionamiento aumentando el numero de elementos, dandoles valores y pesos con doble digito
 
 ### Tercer caso de Prueba
 
-3
-1
-2
-3
-4
-5
-1
-4
+10
+92
+57
+49
+68
+60
+43
+67
+84
+87
+72
+23
+31
+29
+44
+53
+38
+63
+85
+89
+82
+165
 
-Salida:
+Salida: 309
 
-#### Explicación: Utilizamos este caso de prueba para verificar que se encuentra el camino más corto. Incluso cuando el pasar por varias aristas representa menos peso que la arista directamente conectada al vertice objetivo
+#### Explicación: Aumentamos el numero de elementos a diez, y solo utilizamos valores y pesos de doble digito. Tambien para este caso de prueba testeamos el tiempo de ejecucion del programa
 
 ### Cuarto caso de Prueba
 
-3
-1
-2
-3
-4
-5
-1
-4
+15
+135
+139
+149
+150
+156
+163
+173
+184
+192
+201
+210
+214
+221
+229
+240
+70
+73
+77
+80
+82
+87
+90
+94
+98
+106
+110
+113
+115
+118
+120
+750
 
-Salida:
+Salida: 1458
 
-#### Explicación: Utilizamos este caso para verificar su funcionamiento cuando la matriz no tiene ningun infinito. Todas las aristas tienen peso. Así podemos también probar que se encuentra el camino con menos peso. Incluso cuando el pasar por varias aristas es menos pesado que la arista que esta directamente conectada al vertice objetivo
+#### Explicación: Subimos el numero de elementos a quince, tambien utilizamos valores y pesos de tres cifras. Incluyendo tambien que el peso maximo de la mochila es de tres cifras y el profit debio salir de cuatro. Tambien con este caso probamos el tiempo de ejecucion del programa
