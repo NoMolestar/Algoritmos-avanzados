7e8b0b4feaa2d2398f041539db03a93068651fed
Carlos Moisés Chávez Jiménez <carlosmoiseschavezjimenez@gmail.com>
2021-10-30 22:48:57 -0500

    Added base files to the project

---
+++ A01114901-A01637322_Act5.1/readme.md
@@ -0,0 +1,107 @@
+# Triangulación de Delaunay
+
+## Luis Armando Salazar Lopez A01114901
+
+## Carlos Moises Chavez Jimenez A01637322
+
+### Primer Caso de Prueba
+
+3
+1
+2
+3
+4
+5
+1
+4
+
+Salida: 3
+
+#### Explicación: Utilizamos este caso de prueba por ser el de ejemplo en canvas y por ser el caso más basico
+
+### Segundo caso de Prueba
+
+5
+24
+13
+23
+15
+16
+12
+7
+11
+8
+9
+26
+
+Salida: 51
+
+#### Explicación: Probar el funcionamiento aumentando el numero de elementos, dandoles valores y pesos con doble digito
+
+### Tercer caso de Prueba
+
+10
+92
+57
+49
+68
+60
+43
+67
+84
+87
+72
+23
+31
+29
+44
+53
+38
+63
+85
+89
+82
+165
+
+Salida: 309
+
+#### Explicación: Aumentamos el numero de elementos a diez, y solo utilizamos valores y pesos de doble digito. Tambien para este caso de prueba testeamos el tiempo de ejecucion del programa
+
+### Cuarto caso de Prueba
+
+15
+135
+139
+149
+150
+156
+163
+173
+184
+192
+201
+210
+214
+221
+229
+240
+70
+73
+77
+80
+82
+87
+90
+94
+98
+106
+110
+113
+115
+118
+120
+750
+
+Salida: 1458
+
+#### Explicación: Subimos el numero de elementos a quince, tambien utilizamos valores y pesos de tres cifras. Incluyendo tambien que el peso maximo de la mochila es de tres cifras y el profit debio salir de cuatro. Tambien con este caso probamos el tiempo de ejecucion del programa
--- A0XXXXXXX_ActInt2/A0XXXXXXX_ActInt2/main.cpp
+++ A0XXXXXXX_ActInt2/A0XXXXXXX_ActInt2/main.cpp
@@ -32,9 +32,9 @@ int INF = 999999;
 
 //Funcion 1
 //Complejidad: O()
-void cableadoFun1(int n,vector<vector<int>> &distancia, int source)
-{  
-//Complejidad: O(n^2) donde n es la cantidad de vertices
+void cableadoFun1(int n, vector<vector<int>> &distancia, int source)
+{
+    //Complejidad: O(n^2) donde n es la cantidad de vertices
 
     vector<vector<int>> resultados = distancia;
     vector<int> visitados(n, 0); //vector para saber que vertices ya se han visitado
@@ -86,13 +86,85 @@ void cableadoFun1(int n,vector<vector<int>> &distancia, int source)
             cout << "node " << source + 1 << " to node " << k + 1 << " : " << recorrido[k] << endl;
         }
     }
+}
 
+//Función auxiliar que utiliza recursividad para regresar el número de valores de la mochila correspondientes
+//con el peso de los elementos y el peso maximo.
+//Complejidad: 0(2^n) siendo n el numero de elementos en la mochila.
+int auxMochila(vector<int> &valores, vector<int> &pesos, int pesoMax, int size)
+{
+    //Si el peso maximo es más pequeño que cero, regresamos un número negativo grande para indicar
+    //que se ha superado el limite de peso.
+    if (pesoMax < 0)
+    {
+        return -9999999;
+    }
+    //Si se ha alcanzado el limite de peso, o bien, se ha alcanzado el total de numero de elementos, se regresa 0.
+    if (size < 0 || pesoMax == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        //Agregamos el valor a la mochila y le sumamos el valor que sigue en el vector llamando a la funcion
+        //recursivamente. Esto restandole el peso del valor agregado al peso maximo.
+        int agregado = valores[size] + auxMochila(valores, pesos, pesoMax - pesos[size], size - 1);
+        //A la par de lo anterior, ignoramos el valor actual y no lo agregamos. Pasamos al siguiente valor
+        //enviandolo a la recursividad.
+        int noAgregado = auxMochila(valores, pesos, pesoMax, size - 1);
+        //Al final comparamos los resultados de ambos procesos recursivos para obtener el máximo de los valores que
+        //se obtuvieron dentro de la mochila y sin superar el peso maximo.
+        return max(agregado, noAgregado);
+    }
+}
+
+//Función principal que mandá llamar a la auxiliar que será la que regresará los valores dentro
+//de la mochila.
+//Complejidad O(1).
+void mochila(vector<int> &valores, vector<int> &pesos, int pesoMax, int size)
+{
+    //Si el numero de valores es 0, o bien, el peso maximo es 0. Entonces solo imprimimos el 0
+    if (size == 0 || pesoMax == 0)
+    {
+        cout << "0" << endl;
+    }
+    else
+    {
+        //Llamamos a la funcion auxiliar que nos permitira la recursividad
+        cout << endl
+             << auxMochila(valores, pesos, pesoMax, size - 1) << endl;
+    }
 }
 
 //Funcion 2
 //Complejidad: O()
-void rutaFun2(vector<vector<int>> &distancia, int n)
+void rutaFun2(vector<vector<int>> &matriz, int n)
 {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                //Filtramos las celdas que marcan infinito porque indican que no hay arista entre los vertices.
+                if (matriz[j][i] != -1 && matriz[i][k] != -1)
+                {
+                    //Sumamos el peso de dos aristas conectadas para verificar si la sumatoria es menor
+                    //al peso que está marcado en la celda objetivo.
+                    //Nota: En caso de que la celda a compoarar este marcada como infinito, directamente se
+                    //va a sustituir con la sumatoria. Esto porque la condicional if anterior nos inidico
+                    //que existe un camino con más de una arista hacia el vertice objetivo.
+                    if ((matriz[j][i] + matriz[i][k] < matriz[j][k]) || matriz[j][k] == -1)
+                    {
+                        //Si el peso que se obtiene de la suma de ambas aristas es menor al marcado en la celda,
+                        //entonces se subsituye por la sumatoria.
+                        matriz[j][k] = matriz[j][i] + matriz[i][k];
+                    }
+                }
+            }
+        }
+    }
+    imprimir(matriz, n);
 }
 
 //Funcion 3
@@ -100,8 +172,6 @@ void rutaFun2(vector<vector<int>> &distancia, int n)
 void flujoFun3(vector<vector<int>> &datos, int n)
 {
 }
-
-
 
 //Funcion que lee un archivo de texto (recibe el nombre como parametro) y regresa
 //el string completo del contenido del archivo.
@@ -157,17 +227,15 @@ void leerArchivo(const string &name)
             coordenadas.push_back(renglon);
         }
     }
-    file.close();                      //Cierra el archivo
-    for(int i=0;i<colonias;i++){
-        cableadoFun1(colonias,distancia,i);
+    file.close(); //Cierra el archivo
+    for (int i = 0; i < colonias; i++)
+    {
+        cableadoFun1(colonias, distancia, i);
     }
-     //Llamamos a la funcion que mostrara el cableado de las colonias
-    rutaFun2(distancia, colonias);     //Llamamos a la funcion que muestra la ruta a seguir por el personal
-    flujoFun3(capacidad, colonias);    //Llamamos a la funcion que muestra el valor del flujo maximo
+    //Llamamos a la funcion que mostrara el cableado de las colonias
+    rutaFun2(distancia, colonias);  //Llamamos a la funcion que muestra la ruta a seguir por el personal
+    flujoFun3(capacidad, colonias); //Llamamos a la funcion que muestra el valor del flujo maximo
 }
-
-
-
 
 int main()
 {
